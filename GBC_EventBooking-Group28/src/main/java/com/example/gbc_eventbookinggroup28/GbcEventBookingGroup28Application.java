2c2a1475128fe96bf53fcbaafc777a819e24a243
[101435967] <[101435967@georgebrown.ca]>
2024-11-21 16:20:57 -0500

    COMP3095 - Commit

---
+++ GBC_EventBooking-Group28/src/main/java/com/example/gbc_eventbookinggroup28/GbcEventBookingGroup28Application.java
@@ -0,0 +1,13 @@
+package com.example.gbc_eventbookinggroup28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbcEventBookingGroup28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbcEventBookingGroup28Application.class, args);
+    }
+
+}
